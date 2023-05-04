30cf9dadddc31d477ab8eef683294c5a0562bfed
RobotDaniel <101746899+RobotDaniel@users.noreply.github.com>
2023-05-04 16:38:38 +0100

    Add files via upload

---
+++ Person copy/people.py
@@ -0,0 +1,19 @@
+import main
+class People():
+    def __init__(self,name,deck):
+        self.name=name
+        self.deck=deck
+        
+    def getName(self):
+        return self.name
+    def getDeck(self):
+        return self.deck
+    def setName(self,x):
+        self.name = x
+    def setDeck(self,x):
+        self.deck = x
+    main.WriteCards
+
+
+
+
